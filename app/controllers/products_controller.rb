7a161013c1591239a22614d98e60acece3f3bc9b
Peter Jang <peter.x.jang@gmail.com>
2023-08-28 20:43:26 -0500

    Add routes for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def first_product_method
+    @product = Product.first
+    render template: "products/show"
+  end
+
+  def second_product_method
+    @product = Product.second
+    render template: "products/show"
+  end
+
+  def third_product_method
+    @product = Product.third
+    render template: "products/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/all_products", controller: "products", action: "all_products_method"
+  get "/first_product", controller: "products", action: "first_product_method"
+  get "/second_product", controller: "products", action: "second_product_method"
+  get "/third_product", controller: "products", action: "third_product_method"
 end
