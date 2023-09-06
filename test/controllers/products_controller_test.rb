b5b36487a10811ae5657ed9cd19669ec69b6d9dc
Peter Jang <peter.x.jang@gmail.com>
2023-09-05 20:12:49 -0500

    Fix errors with image_url attribute no longer existing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,18 +14,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "image_url", "description", "supplier", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "images", "description", "supplier", "created_at", "updated_at"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { price: 1, name: "test product", description: "test description", image_url: "image.jpg", supplier_id: Supplier.first.id }
+      post "/products.json", params: { price: 1, name: "test product", description: "test description", supplier_id: Supplier.first.id }
       data = JSON.parse(response.body)
       assert_response 200
       refute_nil data["id"]
       assert_equal "test product", data["name"]
       assert_equal "$1.00", data["price"]
-      assert_equal "image.jpg", data["image_url"]
       assert_equal "test description", data["description"]
     end
 
@@ -42,7 +41,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
-    assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
 
     patch "/products/#{product.id}.json", params: { name: "" }
