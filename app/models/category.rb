32827e5458a39ce98736eaac172831a5ab6f8414
Peter Jang <peter.x.jang@gmail.com>
2023-10-02 19:56:34 -0500

    Add has_many through relationships between Category and Product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :category_products
+  has_many :products, through: :category_products
+  # def products
+  #   result = []
+  #   category_products.each do |category_product|
+  #     result << category_product.product
+  #   end
+  #   return result
+  # end
 end
