0ab904c92b18807c33c3969c92465a4483a1202e
Peter Jang <peter.x.jang@gmail.com>
2023-10-04 20:25:35 -0500

    Fix incorrect association

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,7 @@ class Product < ApplicationRecord
 
   has_many :images
   has_many :carted_products
-  has_many :orders, through: carted_products
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, through: :category_products
 
